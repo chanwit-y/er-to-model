80ea9fd5bf39f7dd983f5bfe80e9007d54d57c46
chanwit-y <chanwit.yimneam@hotmail.com>
2022-11-10 20:13:41 +0700

    demo connect node

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,25 +1,63 @@
-import React from "react";
-import ReactFlow, { Background, Controls } from "reactflow";
+import React, { useState, useCallback } from "react";
+import ReactFlow, {
+  Background,
+  Controls,
+  applyEdgeChanges,
+  applyNodeChanges,
+  addEdge
+} from "reactflow";
 
 const initNodes: any = [
   {
     id: "1",
     position: { x: 0, y: 0 },
-    data: {label: "Hello"},
-    type: "input"
+    data: { label: "Hello" },
+    // type: "input", 
   },
   {
     id: "2",
     position: { x: 100, y: 100 },
-    data: {label: "Wrold"},
+    data: { label: "Wrold" },
+  },
+  {
+    id: "3",
+    position: { x: 200, y: 200 },
+    data: { label: "Beer" },
+  },
+  {
+    id: "4",
+    position: { x: 250, y: 250 },
+    data: { label: "Model" },
   },
 ];
 
-const initEdges = [{ id: '1-2', source: '1', target: '2', label: "1...*" }];
+// const initEdges = [{ id: '1-2', source: '1', target: '2', label: "1...*" }];
+const initEdges = [];
 
 export const Test = () => {
+  const [nodes, setNodes] = useState(initNodes);
+  const [edges, setEdges] = useState(initEdges);
+
+  const onNodesChange = useCallback(
+    (change) => setNodes((node) => applyNodeChanges(change, node)),
+    []
+  );
+
+  const onEdgesChange = useCallback(
+    (change) => setEdges((edge) => applyEdgeChanges(change, edge)),
+    []
+  );
+
+  const onConnect = useCallback((params) => setEdges((edge) => addEdge(params, edge)), [])
+
   return (
-    <ReactFlow nodes={initNodes} edges={initEdges}>
+    <ReactFlow
+      nodes={nodes}
+      onNodesChange={onNodesChange}
+      edges={edges}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+    >
       <Background />
       <Controls />
     </ReactFlow>
