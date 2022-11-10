d39d518857e1bf812769f8133e28bcbb3b2ce350
chanwit-y <chanwit.yimneam@hotmail.com>
2022-11-10 20:30:29 +0700

    custom node✌️

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,18 +1,25 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import ReactFlow, {
   Background,
   Controls,
   applyEdgeChanges,
   applyNodeChanges,
-  addEdge
+  addEdge,
 } from "reactflow";
+import { TextUpdaterNode } from "./TextUpdaterNode";
 
 const initNodes: any = [
+  {
+    id: "node-1",
+    type: "textUpdater",
+    position: { x: 300, y: 300 },
+    data: { value: 123 },
+  },
   {
     id: "1",
     position: { x: 0, y: 0 },
     data: { label: "Hello" },
-    // type: "input", 
+    // type: "input",
   },
   {
     id: "2",
@@ -38,6 +45,8 @@ export const Test = () => {
   const [nodes, setNodes] = useState(initNodes);
   const [edges, setEdges] = useState(initEdges);
 
+  const nodeTypes = useMemo(() => ({ textUpdater: TextUpdaterNode }), []);
+
   const onNodesChange = useCallback(
     (change) => setNodes((node) => applyNodeChanges(change, node)),
     []
@@ -48,10 +57,14 @@ export const Test = () => {
     []
   );
 
-  const onConnect = useCallback((params) => setEdges((edge) => addEdge(params, edge)), [])
+  const onConnect = useCallback(
+    (params) => setEdges((edge) => addEdge(params, edge)),
+    []
+  );
 
   return (
     <ReactFlow
+      nodeTypes={nodeTypes}
       nodes={nodes}
       onNodesChange={onNodesChange}
       edges={edges}
---
+++ src/components/TextUpdaterNode.tsx
@@ -0,0 +1,24 @@
+import { useCallback } from 'react';
+import { Handle, Position } from 'reactflow';
+
+const handleStyle = { left: 10 };
+
+export function TextUpdaterNode({ data }) {
+  const onChange = useCallback((evt) => {
+    console.log(evt.target.value);
+  }, []);
+
+  return (
+    <>
+      <Handle type="target" position={Position.Top} />
+      <div>
+        <label htmlFor="text">Text:</label>
+        <input id="text" name="text" onChange={onChange} />
+      </div>
+      <Handle type="source" position={Position.Left} id="a-left" />
+      <Handle type="source" position={Position.Bottom} id="a" />
+      <Handle type="source" position={Position.Bottom} id="b" style={handleStyle} />
+      <Handle type="source" position={Position.Bottom} id="c" style={{left: 40}} />
+    </>
+  );
+}
